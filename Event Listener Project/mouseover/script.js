fa33c6427a3de84337d0a12a88317c095aa19125
Amannroy <royaman56456@gamil.com>
2023-04-23 19:13:53 +0530

    DOM Project Committed

---
+++ Event Listener Project/mouseover/script.js
@@ -0,0 +1,5 @@
+const boxElement = document.getElementById("box");
+
+boxElement.addEventListener("mouseout", function(){
+    alert( "you entered in Restricted Area 🙅 (mouse over event is working properly)");
+});
